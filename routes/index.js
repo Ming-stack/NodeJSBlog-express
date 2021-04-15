7b52338b09f334c682793991ac18a57abd4d7a77
绽放 <1689410894@qq.com>
2021-04-15 18:49:52 +0800

    'debug'

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express')
 var router = express.Router()
 var Article = require('../models/article')
+var Categories = require('../models/categories')
 
 router.get('/', (req, res) => {
     Article.find((err, docs) => {
-        res.render('index', {
-            list: docs
-        });
+        Categories.find((err, docs2)=> {
+            res.render('index', {
+                list: docs,
+                categories: docs2
+            });
+        })
     }).populate('categories')
 })
 router.get('/details/:id', (req, res) => {
